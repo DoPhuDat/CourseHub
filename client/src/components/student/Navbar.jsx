e59ff569b5cdab96ae151cf30c7203ce2e076ecb
dophudat <dophudat2003@gmail.com>
2025-03-14 13:13:38 +0700

    Add CourseList

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,19 +1,19 @@
-import React, { useContext } from "react";
-import { assets } from "../../assets/img/assets";
-import { Link } from "react-router-dom";
-import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
-import { AppContext } from "../../context/AppContext";
+import React, { useContext } from 'react';
+import { assets } from '../../assets/img/assets';
+import { Link } from 'react-router-dom';
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
+import { AppContext } from '../../context/AppContext';
 
 const Navbar = () => {
-  const {navigate} = useContext(AppContext)
-  const isCourseListPage = location.pathname.includes("/course-list");
+  const { navigate } = useContext(AppContext);
+  const isCourseListPage = location.pathname.includes('/course-list');
   const { openSignIn } = useClerk();
   const { user } = useUser();
 
   return (
     <div
       className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:pz-36 border-gray-500 py-4 ${
-        isCourseListPage ? "bg-white" : "bg-cyan-100/70"
+        isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'
       }`}
     >
       <img
