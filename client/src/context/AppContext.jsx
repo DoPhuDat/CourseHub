d3d19c20183656fd765c69421219a9c1ae713abb
dophudat <dophudat2003@gmail.com>
2025-03-15 19:47:59 +0700

    Add Course Detail

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,21 +1,54 @@
-import { createContext, useEffect, useState } from "react";
-import { dummyCourses } from "../assets/img/assets";
-import { useNavigate } from "react-router-dom";
+import { createContext, useEffect, useState } from 'react';
+import { dummyCourses } from '../assets/img/assets';
+import { useNavigate } from 'react-router-dom';
+import humanizeDuration from 'humanize-duration';
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
   const currency = import.meta.env.VITE_CURRENCY;
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [allCourses, setAllCourses] = useState([]);
 
- //fetch all courses
+  //Calculate course chapter time
+  const calculateChapterTime = (chapter) => {
+    let time = 0;
+    chapter.chapterContent.map((lecture) => (time += lecture.lectureDuration));
+    return humanizeDuration(time * 60 * 1000, { units: ['h', 'm'] });
+  };
+
+  //Calculate course  duration
+  const calculateCourseDuration = (course) => {
+    let time = 0;
+    course.courseContent.map((chapter) =>
+      chapter.chapterContent.map((lecture) => (time += lecture.lectureDuration))
+    );
+    return humanizeDuration(time * 60 * 1000, { units: ['h', 'm'] });
+  };
+
+  //Calculate the number of lectures in a course
+
+  const calculateTotalLectures = (course) => {
+    let totalLectures = 0;
+    course.courseContent.forEach((chapter) => {
+      if (Array.isArray(chapter.chapterContent)) {
+        totalLectures += chapter.chapterContent.length;
+      }
+    });
+    return totalLectures;
+  };
+  //fetch all courses
   useEffect(() => {
     setAllCourses(dummyCourses);
   }, []);
   const value = {
-    currency, allCourses,navigate
-  }
+    currency,
+    allCourses,
+    navigate,
+    calculateChapterTime,
+    calculateCourseDuration,
+    calculateTotalLectures,
+  };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
